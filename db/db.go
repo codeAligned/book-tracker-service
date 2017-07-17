65a72f7bb809bb024726005e5bcdab88e6cda9c2
Gigi Sayfan <gigi.sayfan@helix.com>
2017-07-16 22:33:10 -0700

    DB works

--- db/db.go
+++ db/db.go
@@ -2,9 +2,11 @@ package book_tracker
 
 import (
 	"github.com/jmoiron/sqlx"
-	"database/sql"
 	"fmt"
 	"time"
+	. "github.com/the-gigi/book-tracker-service/models"
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 type Config struct {
@@ -23,8 +25,15 @@ type ConfigProvider interface {
 
 
 type DB struct {
-	configProvider ConfigProvider
-	conn *sql.DB
+	configProvider     	 ConfigProvider
+	conn               	 *sqlx.DB
+	books_by_name      	 map[string]*Book
+	categories_by_name 	 map[string]*Category
+	books_by_id      	 map[int]*Book
+	book_name_id_map     map[string]int
+	category_name_id_map map[string]int
+	categories_by_id 	 map[int]*Category
+
 }
 
 func NewDB(configProvider ConfigProvider) *DB {
@@ -34,28 +43,34 @@ func NewDB(configProvider ConfigProvider) *DB {
 }
 
 
-func makePostgressSqlConnectionString(conf Config) string {
+func makePostgressSqlConnectionString(conf Config) (string, string) {
 	t := "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s"
-	return fmt.Sprintf(t, conf.host, conf.port, conf.user, conf.password, conf.dbname, conf.sslmode)
+	return "postgress", fmt.Sprintf(t, conf.host, conf.port, conf.user, conf.password, conf.dbname, conf.sslmode)
 }
 
-func makeSqliteConnectionString(conf Config) string {
-	return conf.filename
+func makeSqliteConnectionString(conf Config) (string, string) {
+	return "sqlite3", conf.filename
 }
 
-func getConnectionString(conf Config) string {
+func getConnectionInfo(conf Config) (driver string, connectionString string, err error) {
+	err = nil
 	if conf.filename != "" {
-		return makeSqliteConnectionString(conf)
+		driver, connectionString = makeSqliteConnectionString(conf)
 	} else {
-		return makePostgressSqlConnectionString(conf)
+		driver, connectionString = makePostgressSqlConnectionString(conf)
 	}
+
+	return
 }
 
 
-func connect(db *DB) error {
+func (db *DB) connect() error {
 	conf, err := db.configProvider.GetConfig()
-	connectionString := getConnectionString(conf)
-	db.conn, err = sql.Open("postgres", connectionString)
+	driver, connectionString, err := getConnectionInfo(conf)
+	if err != nil {
+		return err
+	}
+	db.conn, err = sqlx.Open(driver, connectionString)
 	if err != nil {
 		return err
 	}
@@ -66,26 +81,224 @@ func connect(db *DB) error {
 		return err
 	}
 
+	_, err = db.GetBooks()
+	if err != nil {
+		return err
+	}
+
+	_, err = db.GetCategories()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 
 // Implement the BookTracker interface
-
-func (db *DB) GetBooks() ([]Book, error) {
-
+type dbBook struct {
+	ID int `db:"id"`
+	Name string `db:"name"`
+	ISBN string `db:"isbn"`
+	URL  string `db:"url"`
 }
 
-func (db *DB) GetCategories ([]Category, error) {
+func (db *DB) GetBooks() ([]Book, error) {
+	if db.conn == nil {
+		err := db.connect()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	dbBooks := []dbBook{}
+	err := db.conn.Select(&dbBooks, "SELECT id, name, isbn, url FROM book")
+	if err != nil {
+		return nil, err
+	}
+
+	books := []Book{}
+	db.books_by_id = map[int]*Book{}
+	db.books_by_name = map[string]*Book{}
+	db.book_name_id_map = map[string]int{}
+	for _, b := range dbBooks {
+		book := &Book{
+			Name: b.Name,
+			ISBN: b.ISBN,
+			URL: b.URL,
+		}
+		db.books_by_id[b.ID] = book
+		db.books_by_name[b.Name] = book
+		db.book_name_id_map[b.Name] = b.ID
+		books = append(books, *book)
+	}
+
+	return books, nil
+}
+
+type dbCategory struct {
+	ID int `db:"id"`
+	Name string `db:"name"`
+}
+
+func (db *DB) GetCategories() ([]Category, error) {
+	if db.conn == nil {
+		err := db.connect()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	dbCategories := []dbCategory{}
+	err := db.conn.Select(&dbCategories, "SELECT id, name FROM category")
+	if err != nil {
+		return nil, err
+	}
+
+	categories := []Category{}
+	db.categories_by_id = map[int]*Category{}
+	db.categories_by_name = map[string]*Category{}
+	db.category_name_id_map = map[string]int{}
+	for _, c := range dbCategories {
+		category := &Category{
+			Name: c.Name,
+		}
+		categories = append(categories, *category)
+		db.categories_by_id[c.ID] = category
+		db.categories_by_name[c.Name] = category
+		db.category_name_id_map[c.Name] = c.ID
+	}
+
+	return categories, nil
+}
+
+func (db *DB) getBookByName(name string) (*Book, error) {
+	book, ok := db.books_by_name[name]
+	if ok {
+
+		return book, nil
+	}
+
+	b := dbBook{}
+	err := db.conn.Get(&b, "SELECT * FROM book WHERE NAME='?'", name)
+	if err != nil {
+		return nil, err
+	}
+
+	book = &Book{
+		Name: b.Name,
+		ISBN: b.ISBN,
+		URL: b.URL,
+	}
+	db.books_by_id[b.ID] = book
+	db.books_by_name[name] = book
+ 	
+	return book, nil
+}
+
+
+
+func (db *DB) getCategoryByName(name string) (*Category, error) {
+	category, ok := db.categories_by_name[name]
+	if ok {
+		return category, nil
+	}
+
+	c := dbCategory{}
+	err := db.conn.Get(&c, "SELECT * FROM category WHERE name='?'", name)
+	if err != nil {
+		return nil, err
+	}
+
+	category = &Category{Name: c.Name,}
+	db.categories_by_id[c.ID] = category
+	db.categories_by_name[name] = category
+	db.category_name_id_map[name] = c.ID
+
+	return category, nil
+}
+
+
+type dbRank struct {
+	ID int     			`db:"id"`
+	BookID int     		`db:"book_id"`
+	CategoryID int 		`db:"category_id"`
+	Rank int       		`db:"rank"`
+	Timestamp time.Time `db:"timestamp"`
+	Change int          `db:"change"`
 }
 
 func (db *DB) GetSalesRanks(bookName string,
 							categoryName string,
-							start time.Time,
-							end time.Time) ([]SalesRank, error) {
+							start *time.Time,
+							end *time.Time) ([]SalesRank, error) {
+	if db.conn == nil {
+		err := db.connect()
+		if err != nil {
+			return nil, err
+		}
+	}
 
+	dbRanks := []dbRank{}
+	query := "SELECT * FROM rank"
+	first := true
+	if bookName != "" {
+		query += " WHERE "
+		first = false
+		query += fmt.Sprintf("book_id = %d", db.book_name_id_map[bookName])
+	}
+
+	if categoryName != "" {
+		if first {
+			query += " WHERE "
+			first = false
+		} else {
+			query += " AND "
+		}
+		query += fmt.Sprintf("category_id = %d", db.category_name_id_map[categoryName])
+	}
+
+	if start != nil {
+		if first {
+			query += " WHERE "
+			first = false
+		} else {
+			query += " AND "
+		}
+		query += fmt.Sprintf("timestamp >= '%s'", start)
+	}
+
+	if end != nil {
+		if first {
+			query += " WHERE "
+			first = false
+		} else {
+			query += " AND "
+		}
+		query += fmt.Sprintf("timestamp < '%s'", end)
+	}
+
+	err := db.conn.Select(&dbRanks, query)
+	if err != nil {
+		return nil, err
+	}
+
+	ranks := []SalesRank{}
+	for _, r := range dbRanks {
+		book := db.books_by_id[r.BookID]
+		category := db.categories_by_id[r.CategoryID]
+		ranks = append(ranks, SalesRank{
+			Book: book,
+			Category: category,
+			Rank: r.Rank,
+			Timstamp: r.Timestamp,
+			Change: r.Change,
+		})
+	}
+
+	return ranks, nil
 }
 
-func (db *DB) GetPotentialSales(bookName string, start, end time.Time) ([]PotentialSale, error){
-
+func (db *DB) GetPotentialSales(bookName string, start, end *time.Time) ([]PotentialSale, error){
+	return nil, nil
 }
--- db/db_test.go
+++ db/db_test.go
@@ -4,4 +4,49 @@ package book_tracker
 import (
 	. "github.com/onsi/gomega"
 	"testing"
+	"fmt"
 )
+
+
+type SqliteConfigProvider struct {
+
+}
+
+func (p *SqliteConfigProvider) GetConfig() (Config, error) {
+	config := &Config{
+		filename: "/Users/gigi.sayfan/git/book-tracker/book-tracker.db",
+	}
+
+	return *config, nil
+}
+
+
+func TestGetBooks(t *testing.T) {
+	RegisterTestingT(t)
+
+	db := NewDB(&SqliteConfigProvider{})
+	books, err := db.GetBooks()
+	Ω(err).Should(BeNil())
+	Ω(books).Should(HaveLen(2))
+}
+
+func TestGetCategories(t *testing.T) {
+	RegisterTestingT(t)
+
+	db := NewDB(&SqliteConfigProvider{})
+	categories, err := db.GetCategories()
+	Ω(err).Should(BeNil())
+	Ω(categories).Should(HaveLen(6))
+}
+
+func TestGetSalesRanks	(t *testing.T) {
+	RegisterTestingT(t)
+
+	db := NewDB(&SqliteConfigProvider{})
+	ranks, err := db.GetSalesRanks("", "", nil, nil)
+	Ω(err).Should(BeNil())
+	for _, r := range ranks {
+		fmt.Println(r)
+
+	}
+}
